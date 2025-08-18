51be1593c777f2efffd6191b494c01e17138607d
纪颖志 <jiyingzhi0808@gmail.com>
2025-08-18 10:34:16 +0800

    fix: lint error

--- src/hono/route.ts
+++ src/hono/route.ts
@@ -1,21 +1,27 @@
-import type z from "zod";
-import type { ZodObject } from "zod";
-import type { Provider } from "@/core/di";
+import type z from 'zod'
+import type { ZodObject } from 'zod'
+import type { Provider } from '@/core/di'
 import type {
   Context as BetterContext,
   HandlerReturnType,
   ResponseSchemaMap,
   RouteOptions,
-} from "@/hono/api";
-import type { HttpMethod } from "@/types/common";
+} from '@/hono/api'
+import type { HttpMethod } from '@/types/common'
 
 export function route<
-  ResponseSchema extends ResponseSchemaMap | z.ZodTypeAny | undefined = undefined,
+  ResponseSchema extends
+    | ResponseSchemaMap
+    | z.ZodTypeAny
+    | undefined = undefined,
   ParamsSchema extends ZodObject | undefined = undefined,
   QuerySchema extends ZodObject | undefined = undefined,
   HeadersSchema extends ZodObject | undefined = undefined,
   CookiesSchema extends ZodObject | undefined = undefined,
   BodySchema extends z.ZodTypeAny | undefined = undefined,
+  FormSchema extends z.ZodTypeAny | undefined = undefined,
+  FileSchema extends z.ZodTypeAny | undefined = undefined,
+  FilesSchema extends z.ZodTypeAny | undefined = undefined,
   Deps extends Record<string, Provider<unknown>> | undefined = undefined,
 >(
   def: RouteOptions<
@@ -25,23 +31,32 @@ export function route<
     HeadersSchema,
     CookiesSchema,
     BodySchema,
+    FormSchema,
+    FileSchema,
+    FilesSchema,
     Deps
   >,
 ) {
-  return def;
+  return def
 }
 
 export type RouteDefinition<
-  ResponseSchema extends ResponseSchemaMap | z.ZodTypeAny | undefined = undefined,
+  ResponseSchema extends
+    | ResponseSchemaMap
+    | z.ZodTypeAny
+    | undefined = undefined,
   ParamsSchema extends ZodObject | undefined = undefined,
   QuerySchema extends ZodObject | undefined = undefined,
   HeadersSchema extends ZodObject | undefined = undefined,
   CookiesSchema extends ZodObject | undefined = undefined,
   BodySchema extends z.ZodTypeAny | undefined = undefined,
+  FormSchema extends z.ZodTypeAny | undefined = undefined,
+  FileSchema extends z.ZodTypeAny | undefined = undefined,
+  FilesSchema extends z.ZodTypeAny | undefined = undefined,
   Deps extends Record<string, Provider<unknown>> | undefined = undefined,
 > = {
-  method: HttpMethod;
-  path: string;
+  method: HttpMethod
+  path: string
   handler: (
     ctx: BetterContext<
       ResponseSchema,
@@ -50,9 +65,14 @@ export type RouteDefinition<
       HeadersSchema,
       CookiesSchema,
       BodySchema,
+      FormSchema,
+      FileSchema,
+      FilesSchema,
       Deps
     >,
-  ) => HandlerReturnType<ResponseSchema> | Promise<HandlerReturnType<ResponseSchema>>;
+  ) =>
+    | HandlerReturnType<ResponseSchema>
+    | Promise<HandlerReturnType<ResponseSchema>>
   schema?: RouteOptions<
     ResponseSchema,
     ParamsSchema,
@@ -60,17 +80,26 @@ export type RouteDefinition<
     HeadersSchema,
     CookiesSchema,
     BodySchema,
+    FormSchema,
+    FileSchema,
+    FilesSchema,
     Deps
-  >;
-};
+  >
+}
 
 export function defineRoute<
-  ResponseSchema extends ResponseSchemaMap | z.ZodTypeAny | undefined = undefined,
+  ResponseSchema extends
+    | ResponseSchemaMap
+    | z.ZodTypeAny
+    | undefined = undefined,
   ParamsSchema extends ZodObject | undefined = undefined,
   QuerySchema extends ZodObject | undefined = undefined,
   HeadersSchema extends ZodObject | undefined = undefined,
   CookiesSchema extends ZodObject | undefined = undefined,
   BodySchema extends z.ZodTypeAny | undefined = undefined,
+  FormSchema extends z.ZodTypeAny | undefined = undefined,
+  FileSchema extends z.ZodTypeAny | undefined = undefined,
+  FilesSchema extends z.ZodTypeAny | undefined = undefined,
   Deps extends Record<string, Provider<unknown>> | undefined = undefined,
 >(
   def: RouteDefinition<
@@ -80,8 +109,11 @@ export function defineRoute<
     HeadersSchema,
     CookiesSchema,
     BodySchema,
+    FormSchema,
+    FileSchema,
+    FilesSchema,
     Deps
   >,
 ) {
-  return def;
+  return def
 }
